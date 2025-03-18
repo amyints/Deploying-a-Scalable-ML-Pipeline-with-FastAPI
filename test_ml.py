0a918bb00188acc519e0d56757d3239da74d2504
amyints <amyints@wgu.edu>
2025-03-18 14:57:11 -0400

    removed "['metric]" from test_ml.py and called the correct variables for fbeta, precision, and recall

--- test_ml.py
+++ test_ml.py
@@ -87,7 +87,7 @@ def test_three():
     expected_recall = recall_score(y_true, y_pred, zero_division=1)
     
 
-    assert fbeta == expected_fbeta, f"Expected accuracy: {expected_fbeta}, but got {metrics['fbeta']}"
-    assert precision == expected_precision, f"Expected accuracy: {expected_precision}, but got {metrics['precision']}"
-    assert recall == expected_recall, f"Expected recall: {expected_recall}, but got {metrics['recall']}"
+    assert fbeta == expected_fbeta, f"Expected accuracy: {expected_fbeta}, but got {fbeta}"
+    assert precision == expected_precision, f"Expected accuracy: {expected_precision}, but got {precision}"
+    assert recall == expected_recall, f"Expected recall: {expected_recall}, but got {recall}"
 
